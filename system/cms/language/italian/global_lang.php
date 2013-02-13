c8763f0d3c42269cc23e94183b7cbfc4c86d0085
xavieramoros <xaviamoros@gmail.com#>
2013-02-13 15:57:22 +0100

    Initial commit with pyrocms files

---
+++ system/cms/language/italian/global_lang.php
@@ -0,0 +1,94 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+$lang['global:control-panel'] 			= 'Pannello di Controllo';
+$lang['global:profile']					= 'Profilo'; 
+
+$lang['global:title'] 					= 'Titolo';
+$lang['name_label'] 					= 'Nome';
+$lang['desc_label'] 					= 'Descrizione';
+$lang['version_label'] 					= 'Versione';
+$lang['global:author'] 					= 'Autore';
+$lang['global:action'] 					= 'Azione';
+$lang['global:slug']   					= 'Slug';
+$lang['global:slug_instructions']		= 'Sono ammessi solo caratteri minuscoli e underscore (trattini bassi).';
+
+$lang['global:user'] 					= 'User';
+
+
+$lang['dialog.delete_message'] 			= 'Sei sicuro di volerlo eliminare? L\'azione non può essere annullata.';
+$lang['dialog.confirm'] 				= 'Conferma';
+$lang['dialog.yes'] 					= 'Si';
+$lang['dialog.no'] 						= 'No';
+
+$lang['global:select-none'] 			= '-- Nessuno --';
+$lang['global:select-all'] 				= '-- Tutti --';
+$lang['global:select-any'] 				= '-- Ognuno --';
+$lang['global:select-pick'] 			= '-- Seleziona --';
+
+$lang['global:check-none'] 				= 'Nessuno';
+$lang['global:check-all']				= 'Tutti';
+
+$lang['operators.equal'] 				= 'Uguale a';
+$lang['operators.not_equal'] 			= 'Diverso da';
+$lang['operators.less_than'] 			= 'Minore di';
+$lang['operators.greater_than'] 		= 'Maggiore di';
+
+$lang['navigation_headline'] 			= 'Navigazione';
+$lang['breadcrumb_base_label'] 			= 'Home';
+
+$lang['logged_in_welcome']				= 'Benvenuto %s, ti sei autenticato.';
+$lang['logout_label'] 					= 'Esci';
+$lang['edit_profile_label'] 			= 'Modifica Profilo';
+$lang['settings_label'] 				= 'Impostazioni';
+
+$lang['close_message'] 					= 'Chiudi questo messaggio';
+$lang['general_error_label'] 			= 'Errore';
+$lang['required_error_label'] 			= 'Campo obbligatorio mancante';
+$lang['note_label'] 					= 'Note';
+$lang['success_label'] 					= 'Successo';
+
+$lang['login_title'] 					= 'Entra';
+$lang['cms_fatal_error'] 				= 'Errore fatale, il CMS è installato?';
+$lang['global:demo_restrictions']       = 'Alcune funzionalità di questa pagina sono disabilitate per la demo.';
+
+$lang['login_error_label'] 				= 'Errore di accesso';
+$lang['forgot_password_label'] 			= 'Password dimenticata?';
+$lang['email_label'] 					= 'E-mail';
+$lang['password_label'] 				= 'Password';
+$lang['login_label'] 					= 'Entra';
+
+$lang['help_label'] 					= 'Aiuto';
+$lang['save_label'] 					= 'Salva';
+$lang['cancel_label'] 					= 'Annulla';
+$lang['update_label'] 					= 'Aggiorna';
+$lang['global:delete'] 					= 'Elimina';
+$lang['activate_label'] 				= 'Attiva';
+$lang['approve_label']					= 'Approva';
+$lang['unapprove_label']				= 'Rifiuta';
+$lang['publish_label'] 					= 'Pubblica';
+$lang['upload_label'] 					= 'Carica';
+$lang['global:uninstall'] 				= 'Disinstalla';
+$lang['global:install'] 				= 'Installa';
+$lang['global:enable']					= 'Abilita';
+$lang['global:disable'] 				= 'Disabilita';
+$lang['global:upgrade'] 				= 'Aggiorna';
+
+$lang['all_right_reserved']				= 'Tutti i diritti riservati.'; 
+$lang['powered_by_pyrocms']				= 'Powered by PyroCMS';
+$lang['top_page']						= 'SU'; 
+$lang['back_to_top']					= 'Torna SU';
+
+$lang['global:image'] 					= 'Immagine';
+$lang['global:description'] 		    = 'Descrizione';
+$lang['global:actions']					= 'Azioni';
+$lang['global:view']					= 'Vedi';
+$lang['global:preview']					= 'Anteprima';
+$lang['global:add']						= 'Aggiungi'; 
+$lang['global:edit']					= 'Modifica';
+$lang['global:empty']					= 'Vuoto';
+$lang['global:delete']					= 'Elimina';
+$lang['global:remove']					= 'Elimina';
+
+$lang['global:keywords']				= 'Keywords';
+
+/* End of file main_lang.php */
